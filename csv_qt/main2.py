e2e2445c321f2f771fde54ecec3f638d8a611428
Tornupto100 <slfst117>
2021-12-31 21:18:39 +0100

    Shortened methods through update function Added reset button instead of triple click

--- csv_qt/main2.py
+++ csv_qt/main2.py
@@ -46,73 +46,79 @@ class MainWindow(QtWidgets.QMainWindow):
     def initMe(self):
         #self.data = pd.DataFrame({'Load your csv': []})
         #self.name = "Empty"
-        self.count = 0
+        #self.count = 0
 
         # Initilisiere Fenster
         self.setWindowTitle("CSV_Loader")
         self.setWindowIcon(QIcon('Logo.png'))
-        self.resize(600, 600)  # Größe des Fenster
+        self.resize(800, 600)  # Größe des Fenster
 
+        b_w = 675
         ## Widgets
 
         # Load Button
-        self.button1 = QtWidgets.QPushButton("Upload csv file first", self)
-        self.button1.move(450,50)
-        self.button1.clicked.connect(self.openFileNameDialog)
+        self.load = QtWidgets.QPushButton("Load csv file first!", self)
+        self.load.move(b_w,50)
+        self.load.clicked.connect(self.openFileNameDialog)
 
-        # Pandas Table
+        # Pandas Table + Sorting Functions
         self.table = QtWidgets.QTableView(self)
-        self.table.setGeometry(50, 50, 350, 500)
+        self.table.setGeometry(50, 50, 600, 500)
         self.model = TableModel(self.data)  # Instanziere Daten ? Nur wie?
         self.table.setModel(self.model)
-        #self.table.horizontalHeader().sectionClicked.connect(self.fun)
-
-        #self.table.horizontalHeader(),QtCore.pyqtSignal('sectionClicked (int)'),self.fun
         self.table.horizontalHeader().sectionClicked.connect(self.ascentSort_header)
         self.table.horizontalHeader().sectionDoubleClicked.connect(self.descentSort_header)
-
         #self.table.clicked.connect(self.ascentSort_old())
         #self.table.doubleClicked.connect(self.descentSort_old())
-        # 3 times clicked /
 
         # 2. LineEdit Seperator
         self.line = QLineEdit(self)
         self.line.setMaxLength(1)
         self.line.setPlaceholderText("Enter sep[, or .]")
         self.line.textChanged.connect(self.seperator_added)
-        self.line.move(450, 100)
+        self.line.move(b_w, 100)
 
         # 3. Combobox Delimiter
         self.cb = QComboBox(self)
-        self.cb.move(450, 150)
+        self.cb.move(b_w, 150)
         self.cb.addItems([".", ","])
         self.cb.currentIndexChanged.connect(self.selectionchange)
 
-        # 4.Export Button
-        button3 = QPushButton("Export to xls", self)
-        button3.move(450, 200)
+        # 4. Reset
+        reset = QPushButton("Reset", self)
+        reset.move(b_w, 200)
         #button3.setGeometry(400,400,100,50)
-        button3.clicked.connect(self.save_file)  # Verbindet ein Event hier
+        reset.clicked.connect(self.reset_file)  # Verbindet ein Event hier
+
+        # 5.Export Button
+        export = QPushButton("Export to xls", self)
+        export.move(b_w, 250)
+        #export.setGeometry(400,400,100,50)
+        export.clicked.connect(self.save_file)  # Verbindet ein Event hier
 
     def openFileNameDialog(self):
             #options = QFileDialog.Options()
             #options |= QFileDialog.DontUseNativeDialog
             fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
-                                                      "All Files (*);;CSV Files (*.csv)")
+                                                      "CSV Files (*.csv)")
+
+            #fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+             #                                         "All Files (*);;CSV Files (*.csv)")
             if fileName:
                 #print(fileName)
                 #print("Data is selected")
                 self.name = fileName
-                data=pd.read_csv(fileName)
-                self.model._data = data
-                self.data = data
-                self.model.layoutChanged.emit()
+                self.update_data(pd.read_csv(fileName)) # Only Valid for header files
+
+    def reset_file(self):
+            if self.name:
+                self.update_data(pd.read_csv(self.name))
 
     def seperator_added(self, s): # Decimal Operator --> Integrate default into selection
-        print(s)
-        print(self.line.text())
-        print(self.cb.currentText())
-        print("next")
+        #print(s)
+        #print(self.line.text())
+        #print(self.cb.currentText())
+        #print("next")
 
         if s:
             #print(self.name)
@@ -121,9 +127,6 @@ class MainWindow(QtWidgets.QMainWindow):
             else:
                 data = pd.read_csv(self.name, decimal=s, engine='python')
             self.update_data(data)
-            # self.model._data = data
-            # self.data = data
-            # self.model.layoutChanged.emit()
 
     def selectionchange(self, i): # Delimeter Selector
         print(i)
@@ -133,10 +136,7 @@ class MainWindow(QtWidgets.QMainWindow):
             data = pd.read_csv(self.name, decimal=self.line.text(), delimiter=self.cb.currentText(), engine='python')
         else:
             data = pd.read_csv(self.name, delimiter=self.cb.currentText(), engine='python')
-        #self.update_data(data)
-        self.model._data = data
-        self.data = data
-        self.model.layoutChanged.emit()
+        self.update_data(data)
 
     def save_file(self):
         options = QFileDialog.Options()
@@ -145,18 +145,9 @@ class MainWindow(QtWidgets.QMainWindow):
                                                   "xlsx Files (*.xlsx)", options=options)
         if fileName:
             print(fileName)
-            fileName =fileName + ".xlsx"
+            fileName = fileName + ".xlsx"
             self.data.to_excel(fileName)
             print("Export zu Excel")
-
-
-
-        #print("Export zu Excel")
-        #if self.name != None:
-         #   self.data.to_excel("output.xlsx")
-
-    def counter(self):
-        self.data.sort_values(axis=1)
 
     def update_data(self,data):
         assert isinstance(data, pd.DataFrame)
@@ -176,10 +167,11 @@ class MainWindow(QtWidgets.QMainWindow):
         data = self.data.sort_values(by=header, ascending=False)
         self.update_data(data)
 
-    def ascentSort_old(self, i): # Selection via Fields
+    def ascentSort_old(self, i):
+        # Selection via Fields
         # http://www.python-forum.org/viewtopic.php?f=11&t=16817
         sf = "You clicked on {0}x{1}".format(item.column(), item.row())
-        col=type(item.column())
+        col= type(item.column())
         header=self.data.columns[item.column()][:]
         print(col)
         print("oneClick")
@@ -188,7 +180,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.data = data
         self.model.layoutChanged.emit()
 
-    def descentSort_old(self, item): # Selection via Fields
+    def descentSort_old(self, item):
+        # Selection via Fields, not used
         print("DoubleClick")
         # http://www.python-forum.org/viewtopic.php?f=11&t=16817
         sf = "You clicked on {0}x{1}".format(item.column(), item.row())
@@ -201,7 +194,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.data = data
         self.model.layoutChanged.emit()
 
-"Just a comment"
+
 app=QtWidgets.QApplication(sys.argv)
 window=MainWindow()
 window.show()
